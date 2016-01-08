19a0f83d20a6f7f8a3aaf84ec173d986128d47fd
Scott Olson <scott@solson.me>
2016-01-07 23:09:10 -0600

    Add a game board and basic camera movement and zooming.

--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,119 @@
+extern crate cgmath;
 #[macro_use] extern crate glium;
 extern crate time;
 
+use cgmath::Point2;
 use glium::glutin;
 use glium::backend::glutin_backend::GlutinFacade;
 
-#[derive(Copy, Clone)]
+mod board;
+use board::Board;
+
+#[derive(Clone, Copy, Debug, PartialEq)]
 struct Vertex {
     position: [f32; 2],
 }
+
 implement_vertex!(Vertex, position);
 
-struct GameState {
-    display: GlutinFacade,
-    vertex_buffer: glium::VertexBuffer<Vertex>,
-    indices: glium::index::NoIndices,
-    program: glium::Program,
-
-    last_frame_time: u64,
-    triangle_angle: f32,
-}
-
 /// Actions to take from the game loop.
+#[derive(Clone, Copy, Debug, PartialEq)]
 enum Action {
     None,
     Stop,
 }
 
+const VERTEX_SHADER_SOURCE: &'static str = r#"
+    #version 140
+    in vec2 position;
+    void main() {
+        gl_Position = vec4(position, 0.0, 1.0);
+    }
+"#;
+
+const FRAGMENT_SHADER_SOURCE: &'static str = r#"
+    #version 140
+    out vec4 color;
+    uniform float shade;
+    void main() {
+        color = vec4(shade, shade, shade, 1.0);
+    }
+"#;
+
+struct GameState {
+    display: GlutinFacade,
+    shader_program: glium::Program,
+
+    /// Frames-per-second dependent scaling factor, in units of seconds per frame. For an example
+    /// of its use, an object moving across the screen at `n` board cells per second should move
+    /// `n * time_factor` board cells per frame.
+    time_factor: f32,
+
+    /// Units: nanoseconds.
+    time_last_frame: u64,
+
+    board: Board<bool>,
+    camera_center: Point2<f32>,
+    camera_zoom: f32,
+}
+
+const DEFAULT_ZOOM: f32 = 1.0 / 7.5;
+
 impl GameState {
     fn new() -> Self {
-        let vertex1 = Vertex { position: [-0.5, -0.5] };
-        let vertex2 = Vertex { position: [ 0.0,  0.5] };
-        let vertex3 = Vertex { position: [ 0.5, -0.25] };
-        let shape = vec![vertex1, vertex2, vertex3];
-
         let display = open_window().unwrap();
-        let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
-        let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
-        let program = glium::Program::from_source(&display, VERTEX_SHADER_SOURCE,
-                                                  FRAGMENT_SHADER_SOURCE, None).unwrap();
+        let shader_program = glium::Program::from_source(
+            &display, VERTEX_SHADER_SOURCE, FRAGMENT_SHADER_SOURCE, None).unwrap();
 
         GameState {
             display: display,
-            vertex_buffer: vertex_buffer,
-            indices: indices,
-            program: program,
+            shader_program: shader_program,
 
-            last_frame_time: time::precise_time_ns(),
-            triangle_angle: 0.0,
+            // HACK: Assumes 60 fps. On the other hand, it's only for the first frame.
+            time_factor: 1.0 / 60.0,
+            time_last_frame: time::precise_time_ns(),
+
+            board: Board::new_test_board(),
+            camera_center: Point2::new(4.0, 2.0),
+            camera_zoom: DEFAULT_ZOOM,
         }
     }
 
     fn handle_input(&mut self) -> Action {
+        use glium::glutin::ElementState::*;
+        use glium::glutin::Event::*;
+        use glium::glutin::MouseScrollDelta::*;
+        use glium::glutin::VirtualKeyCode::*;
+
+        // Units: board cells / second
+        let camera_speed = 5.0;
+
         for event in self.display.poll_events() {
             match event {
-                glium::glutin::Event::Closed => return Action::Stop,
+                Closed => return Action::Stop,
+
+                // FIXME: This camera movement code is utterly dumb.
+                KeyboardInput(Pressed, _, Some(key_code)) => match key_code {
+                    Up => {
+                        self.camera_center.y += camera_speed * self.time_factor;
+                    }
+                    Down => {
+                        self.camera_center.y -= camera_speed * self.time_factor;
+                    }
+                    Left => {
+                        self.camera_center.x -= camera_speed * self.time_factor;
+                    }
+                    Right => {
+                        self.camera_center.x += camera_speed * self.time_factor;
+                    }
+                    _ => {}
+                },
+
+                MouseWheel(LineDelta(_, scroll_amount)) => {
+                    // FIXME: Magic numbers.
+                    self.camera_zoom *= 1.1 * scroll_amount;
+                }
+
                 _ => {},
             }
         }
@@ -62,31 +122,51 @@ impl GameState {
     }
 
     fn update(&mut self) {
-        let new_frame_time = time::precise_time_ns();
-        let time_diff_ns = new_frame_time - self.last_frame_time;
-        self.last_frame_time = new_frame_time;
+        let time = time::precise_time_ns();
 
-        self.triangle_angle += time_diff_ns as f32 / 1e9;
+        // Nanoseconds to seconds.
+        self.time_factor = (time - self.time_last_frame) as f32 / 1e9;
+        self.time_last_frame = time;
     }
 
     fn render(&mut self) {
         use glium::Surface;
 
-        let t = self.triangle_angle;
-        let uniforms = uniform! {
-            matrix: [
-                [ t.cos(), t.sin(), 0.0, 0.0],
-                [-t.sin(), t.cos(), 0.0, 0.0],
-                [ 0.0,     0.0,     1.0, 0.0],
-                [ 0.0,     0.0,     0.0, 1.0],
-            ]
-        };
-
         let mut target = self.display.draw();
-        target.clear_color(0.0, 0.0, 0.0, 1.0);
-        target.draw(&self.vertex_buffer, &self.indices, &self.program, &uniforms,
-                    &Default::default()).unwrap();
+        target.clear_color(0.1, 0.1, 0.1, 1.0);
+        let radius = 0.47;
+
+        for i in 0..self.board.width() {
+            for j in 0..self.board.height() {
+                if self.board[j as usize][i as usize] {
+                    let x = i as f32 - self.camera_center.x;
+                    let y = j as f32 - self.camera_center.y;
+                    self.draw_quad(&mut target, x, y, radius, self.camera_zoom, 1.0);
+                }
+            }
+        }
+
+        self.draw_quad(&mut target, 0.0, 0.0, radius, self.camera_zoom / 10.0, 0.5);
         target.finish().unwrap();
+    }
+
+    fn draw_quad(&self, target: &mut glium::Frame, x: f32, y: f32, radius: f32, zoom: f32,
+                 shade: f32) {
+        use glium::Surface;
+
+        // Top/bottom, left/right.
+        let tl = Vertex { position: [(x - radius) * zoom, (y - radius) * zoom] };
+        let tr = Vertex { position: [(x + radius) * zoom, (y - radius) * zoom] };
+        let br = Vertex { position: [(x + radius) * zoom, (y + radius) * zoom] };
+        let bl = Vertex { position: [(x - radius) * zoom, (y + radius) * zoom] };
+        let vertices = [tl, br, tr, tl, bl, br];
+
+        let vertex_buffer = glium::VertexBuffer::new(&self.display, &vertices).unwrap();
+        let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
+        let uniforms = uniform! { shade: shade };
+
+        target.draw(&vertex_buffer, &indices, &self.shader_program, &uniforms,
+                    &Default::default()).unwrap();
     }
 }
 
@@ -94,54 +174,22 @@ fn open_window() -> Result<GlutinFacade, glium::GliumCreationError<glutin::Creat
     use glium::DisplayBuild;
 
     glutin::WindowBuilder::new()
+        .with_dimensions(800, 800)
         .with_title(String::from("Chessrs"))
         .with_vsync()
         .build_glium()
 }
 
-const VERTEX_SHADER_SOURCE: &'static str = r#"
-    #version 140
-
-    in vec2 position;
-
-    uniform mat4 matrix;
-
-    void main() {
-        gl_Position = matrix * vec4(position, 0.0, 1.0);
-    }
-"#;
-
-const FRAGMENT_SHADER_SOURCE: &'static str = r#"
-    #version 140
-
-    out vec4 color;
-
-    void main() {
-        color = vec4(1.0, 0.0, 0.0, 1.0);
-    }
-"#;
-
 fn main() {
     let mut game = GameState::new();
 
-    // let mut frame_count = 0;
-    // let mut frame_start = time_start_ns;
-
     loop {
         match game.handle_input() {
-            Action::Stop => return,
+            Action::Stop => break,
             Action::None => {},
         }
 
         game.update();
         game.render();
-
-        // frame_count += 1;
-        // let frame_seconds = (time_now_ns - frame_start) as f64 / 1e9;
-        // if frame_seconds > 5.0 {
-        //     println!("FPS: {}", frame_count as f64 / frame_seconds);
-        //     frame_count = 0;
-        //     frame_start = time_now_ns;
-        // }
     }
 }
