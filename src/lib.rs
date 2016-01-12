e08f50eeed54f902263d923c4fc41c4fecf1131e
Scott Olson <scott@solson.me>
2016-01-12 17:06:47 -0600

    Update mouse board position every frame.

--- src/lib.rs
+++ src/lib.rs
@@ -26,7 +26,12 @@ pub struct GameState {
     display: Display,
     held_keys: BitSet,
     board: Board<bool>,
-    mouse_position: Point2<f32>,
+
+    /// In OpenGL screen coordinates (in the range `-1.0..1.0`).
+    mouse_screen_pos: Point2<f32>,
+
+    /// In board coordinates.
+    mouse_board_pos: Point2<f32>,
 
     /// Set to the current time in nanoseconds at the beginning of each frame's `update` step.
     time_last_frame: u64,
@@ -43,7 +48,8 @@ impl GameState {
             display: Display::new_window(),
             board: Board::new_test_board(),
             held_keys: BitSet::new(),
-            mouse_position: Point2::origin(),
+            mouse_screen_pos: Point2::origin(),
+            mouse_board_pos: Point2::origin(),
 
             // HACK: Assumes 60 fps. On the other hand, it's only for the first frame.
             time_factor: 1.0 / 60.0,
@@ -72,21 +78,11 @@ impl GameState {
                     self.display.camera.zoom_steps(scroll_amount);
                 }
 
-                MouseMoved((x_pixel, y_pixel)) => {
+                MouseMoved((x, y)) => {
                     // Convert from pixel indices ranging from `0..width` and `0..height` to OpenGL
                     // screen coordinates ranging from `-1.0..1.0`.
-                    let x_screen = 2.0 * x_pixel as f32 / self.display.width as f32 - 1.0;
-                    let y_screen = -2.0 * y_pixel as f32 / self.display.height as f32 + 1.0;
-
-                    // Convert from OpenGL screen coordinates to board coordinates using the
-                    // inverse of the view transformation matrix.
-                    let inv_view = self.display.view_transform().invert().unwrap();
-                    let screen_vec = Vector4::new(x_screen, y_screen, 0.0, 1.0);
-                    let board_vec = inv_view * screen_vec;
-
-                    // FIXME: Record mouse position in raw screen coordinates to update the derived
-                    // board coordinates when panning and zooming while the mouse is stationary.
-                    self.mouse_position = Point2::new(board_vec.x, board_vec.y);
+                    self.mouse_screen_pos.x = 2.0 * x as f32 / self.display.width as f32 - 1.0;
+                    self.mouse_screen_pos.y = -2.0 * y as f32 / self.display.height as f32 + 1.0;
                 }
 
                 _ => {},
@@ -99,20 +95,31 @@ impl GameState {
     pub fn update(&mut self) {
         use glium::glutin::VirtualKeyCode as Key;
 
+        // Update the frame time.
         let time = time::precise_time_ns();
         self.time_factor = (time - self.time_last_frame) as f32 * units::NS_TO_S;
         self.time_last_frame = time;
 
+        // Update the camera position.
         let camera_direction = Vector2 {
             x: self.get_key_direction(Key::Right, Key::Left),
             y: self.get_key_direction(Key::Up, Key::Down),
         };
-
         if camera_direction != Vector2::zero() {
             let frame_step = camera::CAMERA_SPEED * self.time_factor;
             self.display.camera.center = self.display.camera.center
                 + camera_direction.normalize_to(frame_step);
         }
+
+        // Update the mouse board position.
+        //
+        // Convert from OpenGL screen coordinates to board coordinates using the
+        // inverse of the view transformation matrix.
+        let inv_view = self.display.view_transform().invert().unwrap();
+        let screen_vec = Vector4::new(self.mouse_screen_pos.x, self.mouse_screen_pos.y, 0.0, 1.0);
+        let board_vec = inv_view * screen_vec;
+
+        self.mouse_board_pos = Point2::new(board_vec.x, board_vec.y);
     }
 
     // FIXME: Many magic numbers.
@@ -127,8 +134,8 @@ impl GameState {
             for y in 0..self.board.height() {
                 if self.board[y as usize][x as usize] {
                     let point = Point2::new(x as f32, y as f32);
-                    let shade = if (x as f32 - self.mouse_position.x).abs() <= radius &&
-                                   (y as f32 - self.mouse_position.y).abs() <= radius {
+                    let shade = if (x as f32 - self.mouse_board_pos.x).abs() <= radius &&
+                                   (y as f32 - self.mouse_board_pos.y).abs() <= radius {
                         0.7
                     } else {
                         1.0
